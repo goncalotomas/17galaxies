f512aca94637ee9a090dee00cf41ebb5a6fbdeb7
Gonçalo Tomás <goncalo@goncalotomas.com>
2025-01-30 08:37:46 +0000

    Remove errors, still work in progress

--- lib/galaxies/planets/events/research_complete.ex
+++ lib/galaxies/planets/events/research_complete.ex
@@ -3,13 +3,13 @@ defmodule Galaxies.Planets.Events.ResearchComplete do
 
   alias Galaxies.Planets.Events.ResearchEvent
   alias Galaxies.PlayerResearch
+  alias Galaxies.Planet
+  alias Galaxies.Repo
 
   import Ecto.Query
 
-  def process(%PlanetEvent{type: :research_complete} = event, planet_id) do
-    %{
-      research_event: %ResearchEvent{research_id: research_id}
-    } = event
+  def process(%PlanetEvent{type: :research_complete} = event, _planet_id) do
+    %ResearchEvent{player_id: player_id, research_id: research_id} = event.research_event
 
     Ecto.Multi.new()
     |> Ecto.Multi.run(:update_research, fn repo, _changes ->
@@ -24,14 +24,15 @@ defmodule Galaxies.Planets.Events.ResearchComplete do
     |> Ecto.Multi.run(:maybe_start_next_research, fn repo, _changes ->
       research_queue =
         repo.all(
-          from(pe in PlanetEvent,
-            where: not pe.is_processed and not pe.is_cancelled and pe.type == ^:research_complete
-          )
+          from p in Planet,
+            where: p.player_id == ^player_id,
+            join: pe in assoc(p, :events),
+            where: pe.type == ^:research_complete and not pe.is_processed and not pe.is_cancelled,
+            select: pe,
+            order_by: [asc: pe.inserted_at]
         )
-        
-      
 
-      {:ok, nil}
+      {:ok, research_queue}
     end)
     |> Repo.transaction()
   end
