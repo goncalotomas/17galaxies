317300da636abafe4845ff4f6a19db8af9cb5fd3
Gonçalo Tomás <goncalo@goncalotomas.com>
2025-01-30 08:36:48 +0000

    Add player reference in research event to avoid extra query

--- lib/galaxies/planets/events/research_event.ex
+++ lib/galaxies/planets/events/research_event.ex
@@ -9,6 +9,7 @@ defmodule Galaxies.Planets.Events.ResearchEvent do
   @primary_key false
   embedded_schema do
     field :duration_seconds, :integer
+    field :player_id, :binary_id
 
     belongs_to :research, Galaxies.Research
     belongs_to :planet, Galaxies.Planet
@@ -16,6 +17,6 @@ defmodule Galaxies.Planets.Events.ResearchEvent do
 
   def changeset(event, attrs) do
     event
-    |> cast(attrs, [:duration_seconds, :research_id, :planet_id])
+    |> cast(attrs, [:player_id, :duration_seconds, :research_id, :planet_id])
   end
 end
